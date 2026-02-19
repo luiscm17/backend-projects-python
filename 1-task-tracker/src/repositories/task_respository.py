d1c9fe7cbfd24ce6f55b143b2abec2bc3099993d
luis-cm-b460 < 115508520+LuisCM17@users.noreply.github.com>
2026-02-18 21:23:14 -0400

    fix: update repository to use renamed file handler methods

--- 1-task-tracker/src/repositories/task_respository.py
+++ 1-task-tracker/src/repositories/task_respository.py
@@ -9,20 +9,20 @@ class TaskRepository:
 
     def get_next_id(self) -> int:
         """Get next id"""
-        tasks_data = self.file_handler.read_task()
+        tasks_data = self.file_handler.read_tasks()
         if not tasks_data:
             return 1
         return max(task["id"] for task in tasks_data) + 1
 
     def save_task(self, task: Task) -> None:
         """Save task"""
-        tasks_data = self.file_handler.read_task()
+        tasks_data = self.file_handler.read_tasks()
         tasks_data.append(task.to_dict())
-        self.file_handler.write_task(tasks_data)
+        self.file_handler.write_tasks(tasks_data)
 
     def find_by_id(self, task_id: int) -> Optional[Task]:
         """Find task by id"""
-        tasks_data = self.file_handler.read_task()
+        tasks_data = self.file_handler.read_tasks()
         for task_data in tasks_data:
             if task_data["id"] == task_id:
                 task = Task(
@@ -35,7 +35,7 @@ class TaskRepository:
 
     def find_all(self) -> List[Task]:
         """Get all tasks"""
-        tasks_data = self.file_handler.read_task()
+        tasks_data = self.file_handler.read_tasks()
         tasks = []
         for task_data in tasks_data:
             task = Task(task_data["id"], task_data["description"], task_data["status"])
@@ -46,7 +46,7 @@ class TaskRepository:
 
     def find_by_status(self, status: str) -> List[Task]:
         """Get task by status"""
-        tasks_data = self.file_handler.read_task()
+        tasks_data = self.file_handler.read_tasks()
         tasks = []
         for task_data in tasks_data:
             if task_data["status"] == status:
@@ -60,21 +60,21 @@ class TaskRepository:
 
     def update_task(self, task: Task) -> None:
         """Update task"""
-        tasks_data = self.file_handler.read_task()
+        tasks_data = self.file_handler.read_tasks()
 
         for i, task_data in enumerate(tasks_data):
             if task_data["id"] == task.id:
                 tasks_data[i] = task.to_dict()
                 break
-        self.file_handler.write_task(tasks_data)
+        self.file_handler.write_tasks(tasks_data)
 
     def delete_task(self, task_id: int) -> bool:
         """Delete task by id"""
-        tasks_data = self.file_handler.read_task()
+        tasks_data = self.file_handler.read_tasks()
 
         for i, task_data in enumerate(tasks_data):
             if task_data["id"] == task_id:
                 del tasks_data[i]
-                self.file_handler.write_task(tasks_data)
+                self.file_handler.write_tasks(tasks_data)
                 return True
         return False
