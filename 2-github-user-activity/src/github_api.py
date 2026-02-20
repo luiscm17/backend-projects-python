f1faeeb050a77bb6798feaf376755b2e90937000
luis-cm-b460 < 115508520+LuisCM17@users.noreply.github.com>
2026-02-20 01:16:21 -0400

    feat: integrate cache with GitHub API service

--- 2-github-user-activity/src/github_api.py
+++ 2-github-user-activity/src/github_api.py
@@ -1,9 +1,18 @@
 import urllib.request
 import urllib.error
 import json
+from cache import load_cache, save_cache
 
 
 def gh_activity(username):
+    """Get the GitHub activity for a username
+
+    Args:
+        username (str): GitHub username
+
+    Returns:
+        list: List of GitHub events
+    """
     url = f"https://api.github.com/users/{username}/events"
 
     headers = {"User-Agent": "Github-Activity-CLI"}
@@ -20,3 +29,24 @@ def gh_activity(username):
         return f"Error: {e.code} - {e.reason}"
     except urllib.error.URLError as e:
         return f"Error: {e.reason}"
+
+
+def get_user_activity(username):
+    """Get the GitHub activity for a username
+
+    Args:
+        username (str): GitHub username
+
+    Returns:
+        list: List of GitHub events
+    """
+    cache_data = load_cache(username)
+    if cache_data:
+        print("Using cached data...")
+        return cache_data
+    data = gh_activity(username)
+
+    if not isinstance(data, str):
+        save_cache(username, data)
+
+    return data
